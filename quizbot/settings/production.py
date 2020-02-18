0ab3b8c6f74be072f1cb10ff83502bd3cd51286a
Leonid Gubarkov <l.gubarkov@splunk.local>
2020-02-18 07:13:37 +0300

    added production settings

--- quizbot/settings/production.py
+++ quizbot/settings/production.py
@@ -4,3 +4,20 @@ from .settings import *
 ALLOWED_HOSTS = ['app']
 DEBUG = False
 PRODUCTION = True
+
+
+def env(name, default=None, type=None):
+    value = os.environ.get(name, default)
+    return type(value) if type is not None else value
+
+
+DEFAULT_BOT_TOKEN = env('BOT_TOKEN', None)
+
+TG_WEBHOOK = os.environ.get('BOT_WEBHOOK') == 'true'
+TG_WEBHOOK_PORT = int(os.environ.get('BOT_WEBHOOK_PORT', 5555))
+
+
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
