98d75db818d3d09000ee4d59688a29223953b7ec
Leonid Gubarkov <l.gubarkov@splunk.local>
2020-02-27 17:25:08 +0300

    fixed morpho with multiple words

--- polls/templatetags/morpho_extras.py
+++ polls/templatetags/morpho_extras.py
@@ -8,10 +8,11 @@ morph = pymorphy2.MorphAnalyzer()
 
 @register.filter
 @stringfilter
-def smart_plural(value):
+def smart_plural(value, number):
     words = str(value).split()
-    word = morph.parse(words.pop())[0]
-    word = word.inflect({'plur', 'gent'}).word
-    words.append(word if len(words) > 0 else word.capitalize())
+    for i in range(len(words)):
+        word = morph.parse(words[i])[0]
+        word = word.make_agree_with_number(number).word
+        words[i] = word if i > 1 else word.capitalize()
 
     return " ".join(words)
